1bf523f726ab662931dd77d1e36495bb916b221b
Harish Chetty <chettyharish@gmail.com>
2015-05-22 14:34:08 -0400

    Final?

---
+++ killer.sh
@@ -0,0 +1,10 @@
+ssh linprog1 'pkill -u chetty'
+ssh linprog2 'pkill -u chetty'
+ssh linprog3 'pkill -u chetty'
+ssh linprog4 'pkill -u chetty'
+ssh program1 'pkill -u chetty'
+ssh program2 'pkill -u chetty'
+ssh program3 'pkill -u chetty'
+ssh program4 'pkill -u chetty'
+ssh shell 'pkill -u chetty'
+
