d45df401ecb90cae5e69bd03c293faa06cdb627e
Harish Chetty <chettyharish@gmail.com>
2015-06-10 14:03:38 -0400

    s

--- temp.c
+++ temp.c
@@ -7,5 +7,9 @@ int main() {
 	argv[0] = "/usr/bin/cat";
 	argv[1] = "Makefile";
 	argv[2] = NULL;
+
+	printf("%d\t\t%s\n",0,argv[0]);
+	printf("%d\t\t%s\n",1,argv[1]);
+	printf("%d\t\t%s\n",2,argv[2]);
 	execv(argv[0], argv);
 }
