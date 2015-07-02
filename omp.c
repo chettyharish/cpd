9e490e62c5fe18139283dee118882b0836b5fbec
Harish Chetty <chettyharish@gmail.com>
2015-07-02 01:04:21 -0400

    omp

--- omp.c
+++ omp.c
@@ -107,8 +107,9 @@ int main(int argc, char *argv[]) {
 	if (DEBUG_LEVEL > 10)
 		print_world();
 
+	int chunk_size = 100;
 	for (iter = 0; (iter < 200) && (count < 50 * init_count) && (count > init_count / 50); iter++) {
-#pragma omp parallel for private(x,y,c) shared(neww,w,w_X,w_Y)
+#pragma omp parallel for private(x,y,c) shared(neww,w,w_X,w_Y) schedule(static , chunk_size) collapse(2)
 		for (x = 0; x < w_X; x++) {
 			for (y = 0; y < w_Y; y++) {
 				c = neighborcount(x, y); /* count neighbors */
