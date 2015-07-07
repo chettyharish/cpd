935220eeb3cb94da76431c169c6deee27ab1d475
Harish Chetty <chettyharish@gmail.com>
2015-07-07 15:28:42 -0400

    Modified

--- temp.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-if __name__ == "__main__":
-    for i in range(1,100):
-        cmd = "time ./omp 1427 1553 " + str(i) + " >> tempomp "
-        print(cmd)
-        os.system(cmd)
-        print("")
