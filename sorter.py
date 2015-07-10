37ee225da0ddb9cae2bc691b06c75c387583ab0c
Harish Chetty <chettyharish@gmail.com>
2015-07-10 05:35:02 -0400

    Trying weird studd

--- sorter.py
+++ sorter.py
@@ -1,7 +1,16 @@
 import random
 import sys
 import time
+import threading
+import math
 
+
+
+
+
+maxlen = False
+num_threads = 32
+threads = []
 
 def issorted(data):
     for i in range(len(data) - 1):
@@ -9,6 +18,42 @@ def issorted(data):
             return False
 
     return True
+
+def modder(tid, div, data, base, buckets):
+#     print("Thread =  : " + str(tid))
+    global maxlen , num_threads
+    ele_per_blk = math.ceil(len(data) / num_threads)
+    for i in range(tid * ele_per_blk, (tid + 1) * ele_per_blk):
+        if(i >= len(data)):
+            return
+        temp = int(data[i] / div)
+        buckets[temp % base].append((i, data[i]))
+        if maxlen and temp > 0:
+            maxlen = False
+    return
+
+
+def radixsort_parallel(data):
+    base, pos, div = (10, -1, 1)
+    global maxlen , num_threads
+
+    while (maxlen == False):
+        maxlen = True
+        buckets = [list() for _ in range(base)]
+
+        for tid in range(num_threads):
+            t = threading.Thread(
+                target=modder, args=(tid, div, data, base, buckets))
+            threads.append(t)
+            t.start()
+
+        div *= base
+
+        k = 0
+        for i in range(base):
+            for j in sorted(buckets[i]):
+                data[k] = j[1]
+                k += 1
 
 
 def radixsort(data):
@@ -27,7 +72,6 @@ def radixsort(data):
 
         div *= base
 
-# Not parallelizable directly
         k = 0
         for i in range(base):
             for j in buckets[i]:
@@ -44,17 +88,21 @@ if __name__ == "__main__":
     start = time.time()
     size = int(float(sys.argv[1]))
     print(8 * 10e9 / size)
-    A = random.sample(range(0,10**10) , size)
+    A = random.sample(range(0, 10**10), size)
     curr = time.time()
     print("FIRST : " + str(curr - start))
     if int(sys.argv[2]) == 0:
         print("RadixSort")
         radixsort(A)
+    elif int(sys.argv[2]) == 1:
+        print("RadixSort Parallel")
+        radixsort_parallel(A)
     else:
         print("TimSort")
         sorted(A)
     print("END : " + str(time.time() - curr))
-
+    
+#     print(A)
     if(issorted(A)):
         print("Sorted Correctly")
     else:
